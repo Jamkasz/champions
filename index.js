60709edcdb53a1cc1ef65645fa24b33230e3729b
Joel Ortiz <l3go.joo@gmail.com>
2024-07-13 13:30:07 +0100

    core functionality

---
+++ index.js
@@ -0,0 +1,55 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-database.js"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA6uQNIRA_5yjqn1h7sWbWIDpxFn_v65Uk",
+    authDomain: "realtime-database-c1c98.firebaseapp.com",
+    databaseURL: "https://realtime-database-c1c98-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "realtime-database-c1c98",
+    storageBucket: "realtime-database-c1c98.appspot.com",
+    messagingSenderId: "234792289299",
+    appId: "1:234792289299:web:850db364ecffa21ed22be6"
+}
+
+const app = initializeApp(firebaseConfig)
+const database = getDatabase(app)
+const endorsementsInDB = ref(database, "endorsements")
+
+const publishBtn = document.getElementById("publish-btn")
+const inputEl = document.getElementById("input-el")
+const endorsementListEl = document.getElementById("endorsement-list-el")
+
+publishBtn.addEventListener("click", function () {
+    addEndorsement(inputEl.value)
+    clearInputEl()
+})
+
+function addEndorsement(value) {
+    push(endorsementsInDB, value)
+}
+
+function clearInputEl() {
+    inputEl.value = ""
+}
+
+function clearEndorsementList() {
+    endorsementListEl.innerHTML = ""
+}
+
+function appendItemToList(item) {
+    let newItemEl = document.createElement("p")
+    newItemEl.textContent = item[1]
+    endorsementListEl.append(newItemEl)
+}
+
+onValue(endorsementsInDB, function (snapshot) {
+    clearEndorsementList()
+    
+    if (snapshot.exists()) {
+        const itemsArray = Object.entries(snapshot.val())
+    
+        for (let i = 0; i < itemsArray.length; i++) {
+            appendItemToList(itemsArray[i])
+        }
+    }
+})
